e0e42c401976db07f95eacf857c3dd809eaa37fa
Jack Lam <jacklam718@gmail.com>
2016-08-27 17:03:32 +0800

    remove useless files for gh-pages

--- index.js
+++
@@ -1,6 +0,0 @@
-import ButtonComponent from './src/ButtonComponent';
-
-export Colors from './src/common/Colors';
-export AnimatedButton from './src/common/Button';
-export InnerButton from './src/common/InnerButton';
-export default ButtonComponent;
