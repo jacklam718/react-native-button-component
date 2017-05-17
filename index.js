c7d0fe58f953f4bb3d78c07c7d20a10c0467becb
agentile <agentilela@gmail.com>
2017-05-17 16:55:30 -0700

    fixed default export

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import ButtonComponent from './src/ButtonComponent';
-
 export * from './src/common/Button';
 export * from './src/common/InnerButton';
 
 export * from './src/CircleButton';
 export * from './src/RoundButton';
 export * from './src/RectangleButton';
-export default ButtonComponent;
+export default './src/ButtonComponent';
