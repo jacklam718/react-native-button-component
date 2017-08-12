6e371edf00e5d7f3addb7f8070a77b83d3382a63
Jack Lam <jacklam718@gmail.com>
2017-08-12 20:18:27 +0800

    add tests and Travis CI config

---
+++ __tests__/ButtonComponent.test.js
@@ -0,0 +1,18 @@
+import { View } from 'react-native';
+import React from 'react';
+import ButtonComponent from '../src';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <ButtonComponent
+      text="Icon Buttons"
+      onPress={() => {
+      }}
+    />,
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
